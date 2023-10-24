56ecdd06bdfaf73de9a312d86684a8a8e82b3932
SimonBoothroyd <simon.boothroyd@hotmail.com>
2023-10-24 08:51:54 -0400

    Fix LM optimizer on CUDA devices (#83)

--- smee/optimizers/__init__.py
+++ smee/optimizers/__init__.py
@@ -1,5 +1,5 @@
 """Custom parameter optimizers."""
 
-from smee.optimizers._lm import LevenbergMarquardtConfig, levenberg_marquardt
+from smee.optimizers._lm import LevenbergMarquardt, LevenbergMarquardtConfig
 
-__all__ = ["LevenbergMarquardtConfig", "levenberg_marquardt"]
+__all__ = ["LevenbergMarquardt", "LevenbergMarquardtConfig"]
