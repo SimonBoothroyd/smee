c01d912e7fa5b55719b135d31399d0a948a128a9
SimonBoothroyd <simon.boothroyd@hotmail.com>
2024-11-09 19:47:27 -0500

    Add support for computing dG gradient w.r.t solvent params (#121)

--- smee/tests/mm/test_fe.py
+++ smee/tests/mm/test_fe.py
@@ -31,21 +31,21 @@ def load_systems(solute: str, solvent: str):
 
 @pytest.mark.fe
 def test_fe_ops(tmp_cwd):
-    # taken from a run on commit 7915d1e323318d2314a8b0322e7f44968c660c21
+    # taken from a run on commit ec3d272b466f761ed838e16a5ba7b97ceadc463b
     expected_dg = torch.tensor(-3.8262).double()
     expected_dg_dtheta = torch.tensor(
         [
-            [1.0288e01],
-            [1.3976e01],
-            [2.6423e01],
-            [9.1453e00],
-            [9.0158e00],
-            [9.5534e00],
-            [1.0414e01],
-            [1.1257e01],
-            [-4.0618e00],
-            [5.0233e03],
-            [-1.3574e03],
+            [10.2679],
+            [13.3933],
+            [25.3670],
+            [9.3747],
+            [9.3279],
+            [9.1520],
+            [10.5614],
+            [9.6908],
+            [-4.4326],
+            [-17.3971],
+            [-38.5407],
         ]
     ).double()
 
@@ -62,11 +62,17 @@ def test_fe_ops(tmp_cwd):
     dg = smee.mm.compute_dg_solv(ff, output_dir)
     dg_dtheta = torch.autograd.grad(dg, params)[0]
 
+    print("dg COMP", dg, flush=True)
+    print("dg_dtheta COMP", dg_dtheta, dg, flush=True)
+
     assert dg == pytest.approx(expected_dg, abs=0.5)
     assert dg_dtheta == pytest.approx(expected_dg_dtheta, rel=1.1)
 
     dg, n_eff = smee.mm.reweight_dg_solv(ff, output_dir, dg)
     dg_dtheta = torch.autograd.grad(dg, params)[0]
+
+    print("dg REWEIGHT", dg, flush=True)
+    print("dg_dtheta REWEIGHT", dg_dtheta, dg, flush=True)
 
     assert dg == pytest.approx(expected_dg, abs=0.5)
     assert dg_dtheta == pytest.approx(expected_dg_dtheta, rel=1.1)
